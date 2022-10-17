13c03a69a5d5aa2c7b2aa934c1ae93443a4bd846
sourabh agrawal <sourabhkhs23@gmail.com>
2022-10-18 00:47:06 +0530

    Limiting middleware execution to POST

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/add-product',(req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
-
+//delete,put,patch,use can be used instead of post:
 app.post('/product', (req,res,next) => {
     console.log(req.body);
     res.redirect('/');
