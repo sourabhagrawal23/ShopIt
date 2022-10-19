dc59b8c3421e6c6682cb609d81c4927c8d761754
sourabh agrawal <sourabhkhs23@gmail.com>
2022-10-19 18:09:50 +0530

    changed use to get

--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,9 @@ const express = require('express');
 //This route is like a mini Express app which can be tied to the other Express apps
 const router = express.Router();
 
-router.use('/', (req,res,next) => {
+router.get('/', (req,res,next) => {
     console.log('This always runs!');
-    //res.send('<h1>Common</h1>')
+    res.send('<h1>Common</h1>')
 });
 
 module.exports = router;
