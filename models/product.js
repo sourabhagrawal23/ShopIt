a2f506f2293cb744e7a4ebbe29c0bc237762fd01
sourabh agrawal <sourabhkhs23@gmail.com>
2022-11-05 17:16:14 +0530

    Refactored the file storage code - MVC Pattern Implemented

--- models/product.js
+++ models/product.js
@@ -1,5 +1,17 @@
 const fs = require('fs');
 const path = require('path');
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json'); 
+
+const getProductsFromFile = (cb) => {
+        fs.readFile(p, (err,fileContent) => {
+            if(err){
+                cb([]);
+            }
+            else{
+                cb(JSON.parse(fileContent));
+            }
+        })
+}
 
 // const products = [];
 
@@ -10,26 +22,15 @@ module.exports = class Product {
     
     save() {
         //products.push(this);
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json'); 
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if(!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile( (products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
-        });
-    }
+    });
+}
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, (err,fileContent) => {
-            if(err){
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        })
+        getProductsFromFile(cb);
     }
 }
