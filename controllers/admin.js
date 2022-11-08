c653aadc314cb31a4d7de53ca287af92b0a6238c
sourabh agrawal <sourabhkhs23@gmail.com>
2022-11-08 15:32:53 +0530

    Edit functionality added

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
     //send method automatically sets content type to HTML
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-    res.render('admin/edit-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
+    res.render('admin/edit-product', {pageTitle: 'Add Product', path: '/admin/add-product', editing: false})
 }
 
 
@@ -16,7 +16,7 @@ exports.postAddProduct = (req,res,next) => {
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(title, imageUrl, price, description);
+    const product = new Product(null, title, imageUrl, price, description);
     product.save();
 
     res.redirect('/');
@@ -29,9 +29,32 @@ exports.getEditProduct = (req, res, next) => {
     if(!editMode){
         return res.redirect('/');
     }
-    res.render('admin/edit-product', {pageTitle: 'Edit Product', path: '/admin/edit-product', editing:true})
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if(!product)
+        {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {pageTitle: 'Edit Product', path: '/admin/edit-product', editing:true, product:product})
+    })
 }
 
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+    const updatedProduct = new Product(
+      prodId,
+      updatedTitle,
+      updatedImageUrl,
+      updatedDesc,
+      updatedPrice
+    );
+    updatedProduct.save();
+    res.redirect('/admin/products');
+  };
 
 exports.getProducts = (req,res,next) => {
     Product.fetchAll( products => {
