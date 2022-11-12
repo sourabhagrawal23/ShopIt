23d8f127fa6fcaa083d503b3bd23cc6ff203a084
sourabh agrawal <sourabhkhs23@gmail.com>
2022-11-12 14:15:29 +0530

    Dynamic routes added and delete functionality implemented

--- controllers/admin.js
+++ controllers/admin.js
@@ -61,3 +61,10 @@ exports.getProducts = (req,res,next) => {
         res.render('admin/products', { prods: products, pageTitle: 'Admin Products', path: '/admin/products' });
     })
 }
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteById(prodId);
+    res.redirect('/admin/products');
+  };
+  
