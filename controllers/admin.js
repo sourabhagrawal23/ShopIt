d474600f3ac6333da198d6d6a29c328e0b8dc8d8
sourabh agrawal <sourabhkhs23@gmail.com>
2022-11-13 12:27:50 +0530

    Deletion implemented using Sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -31,6 +31,7 @@ exports.postAddProduct = (req,res,next) => {
     })
     .then((result)=> {
         console.log('Created product');
+        res.redirect('/admin/products');
     })
     .catch(err => {
         console.log(err);
@@ -100,7 +101,15 @@ exports.getProducts = (req,res,next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId);
-    res.redirect('/admin/products');
+    // Product.deleteById(prodId);
+    Product.findByPk(prodId)
+    .then(product => {
+        return product.destroy();
+    })
+    .then(result => {
+        res.redirect('/admin/products');
+        console.log("DESTROYED PRODUCT");
+    })
+    .catch(err => console.log(err));
   };
   
