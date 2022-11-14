53bf4ced3445ba25833fedf72be4091098514f3f
sourabh agrawal <sourabhkhs23@gmail.com>
2022-11-15 00:26:37 +0530

    Delete and Edit functionalities implemented using mongodb

--- controllers/admin.js
+++ controllers/admin.js
@@ -28,26 +28,26 @@ exports.postAddProduct = (req,res,next) => {
     })
 };
 
-// exports.getEditProduct = (req, res, next) => {
-//     //send method automatically sets content type to HTML
-//     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-//     const editMode = req.query.edit;
-//     if(!editMode){
-//         return res.redirect('/');
-//     }
-//     const prodId = req.params.productId;
-//     req.user
-//     .getProducts({ id: prodId })
-//     // Product.findByPk(prodId)
-//     .then(products => {
-//         const product = products[0];
-//         if(!product)
-//         {
-//             return res.redirect('/');
-//         }
-//         res.render('admin/edit-product', {pageTitle: 'Edit Product', path: '/admin/edit-product', editing:true, product:product})
-//     });
-// };
+exports.getEditProduct = (req, res, next) => {
+    //send method automatically sets content type to HTML
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    const editMode = req.query.edit;
+    if(!editMode){
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    // req.user
+    // .getProducts({ id: prodId })
+    // Product.findByPk(prodId)
+    Product.findById(prodId)
+    .then(product => {
+        if(!product)
+        {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {pageTitle: 'Edit Product', path: '/admin/edit-product', editing:true, product:product})
+    });
+};
 
 exports.postEditProduct = (req, res, next) => {
     const prodId = req.body.productId;
@@ -65,14 +65,24 @@ exports.postEditProduct = (req, res, next) => {
     //   updatedPrice
     // );
 
-    Product.findByPk(prodId)
-    .then(product => {
-        product.title = updatedTitle;
-        product.price = updatedPrice;
-        product.description = updatedDesc;
-        product.imageUrl = updatedImageUrl;
-        return product.save();
-    })
+    const product = new Product(
+        updatedTitle,
+        updatedPrice,
+        updatedDesc,
+        updatedImageUrl,
+        prodId
+    );
+
+    // Product.findById(prodId)
+    // .then(product => {
+    //     product.title = updatedTitle;
+    //     product.price = updatedPrice;
+    //     product.description = updatedDesc;
+    //     product.imageUrl = updatedImageUrl;
+    //     return product.save();
+    // })
+    product
+    .save()
     .then(result => {
         res.redirect('/admin/products');
         console.log('UPDATED PRODUCT');
@@ -86,24 +96,22 @@ exports.getProducts = (req,res,next) => {
     // });
 
     // Product.findAll()
-    req.user.getProducts()
+    // req.user.getProducts()
+    Product.fetchAll()
     .then( products => {
         res.render('admin/products', { prods: products, pageTitle: 'Admin Products', path: '/admin/products' });
     })
     .catch(err => console.log(err));
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-//     const prodId = req.body.productId;
-//     // Product.deleteById(prodId);
-//     Product.findByPk(prodId)
-//     .then(product => {
-//         return product.destroy();
-//     })
-//     .then(result => {
-//         res.redirect('/admin/products');
-//         console.log("DESTROYED PRODUCT");
-//     })
-//     .catch(err => console.log(err));
-//   };
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    // Product.deleteById(prodId);
+    Product.deleteById(prodId)
+    .then(result => {
+        res.redirect('/admin/products');
+        console.log("DESTROYED PRODUCT");
+    })
+    .catch(err => console.log(err));
+  };
   
--- models/product.js
+++ models/product.js
@@ -2,17 +2,27 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    return db.collection('products')
-    .insertOne(this)
+    let dbOp;
+    if(this._id) {
+      dbOp = db
+      .collection('products')
+      .updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: this});
+    }
+    else{
+      dbOp = db.collection('products')
+      .insertOne(this);
+    }
+    return dbOp
     .then(result => {console.log(result)})
     .catch(err => {console.log(err)});
   }
@@ -33,6 +43,15 @@ class Product {
     .find({_id: new mongodb.ObjectId(prodId)})
     .next()
     .then(product => {return product})
+    .catch(err => {console.log(err)});
+  }
+
+  static deleteById(prodId)
+  {
+    const db = getDb();
+    db.collection('products')
+    .deleteOne({_id: new mongodb.ObjectId(prodId)})
+    .then(result => {console.log("DELETED")})
     .catch(err => {console.log(err)});
   }
 }
