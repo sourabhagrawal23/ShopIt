f4d78192dec2b529a1f76216c12836a0f15b41f9
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-04 22:48:15 +0530

    Password reset functionality implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer')
@@ -114,7 +115,7 @@ exports.postSignup = (req, res, next) => {
                             html: '<h1>You successfully signed up!</h1>'
                         });
                     })
-                    .catch (err => {
+                    .catch(err => {
                         console.log(err);
                     })
             })
@@ -129,3 +130,109 @@ exports.postLogout = (req, res, next) => {
         res.redirect('/');
     })
 };
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    }
+    else {
+        message = null;
+    }
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset Password',
+        errorMessage: message
+    });
+};
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, Buffer) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/reset');
+        }
+        const token = Buffer.toString('hex');
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    req.flash('error', 'No account with that email found!');
+                    return res.redirect('/reset');
+                }
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now() + 3600000;
+                return user.save();
+            })
+            .then(result => {
+                res.redirect('/');
+                console.log('http://localhost:3000/reset/' + token);
+                transporter.sendMail({
+                    to: req.body.email,
+                    from: 'sourabhkhs23@gmail.com',
+                    subject: 'Password reset',
+                    html:
+                        `
+                    <p> You requested password reset </p>
+                    <P> Click this <a href="http://localhost:3000/reset/${token}"></a>link to set a new password </p>
+                `
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    })
+}
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+        .then(user => {
+
+            let message = req.flash('error');
+            if (message.length > 0) {
+                message = message[0];
+            }
+            else {
+                message = null;
+            }
+
+            res.render('auth/new-password', {
+                path: '/new-password',
+                pageTitle: 'New Password',
+                errorMessage: message,
+                userId: user._id.toString(),
+                passwordToken: token
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+exports.postNewPassword = (req, res, next) => {
+    const NewPassword = req.body.password;
+    const userId = req.body.userId;
+    const passwordToken = req.body.passwordToken;
+    let resetUser;
+
+    User.findOne({
+        resetToken: passwordToken, resetTokenExpiration: { $gt: Date.now() },
+        _id: userId
+    })
+        .then(user => {
+            resetUser = user;
+            return bcrypt.hash(NewPassword,12);
+        })
+        .then(hashedPassword => {
+            resetUser.password = hashedPassword;
+            resetUser.resetToken = undefined;
+            resetUser.resetTokenExpiration = undefined;
+            return resetUser.save();
+        })
+        .then(result => {
+            res.redirect('/login');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
