90117334a6130a391b1cbdfc26e90a50c9ce396f
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-04 14:27:15 +0530

    Flash messages implemented

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
+const flash = require('connect-flash');
 
 
 const errorController = require('./controllers/error');
@@ -52,6 +53,7 @@ app.use(
 })
 );
 app.use(csrfProtection);
+app.use(flash());
 
 // app.use((req, res, next) => {
 //     User.findById('637b8ce105699be0c5cf7817')
--- controllers/auth.js
+++ controllers/auth.js
@@ -9,19 +9,35 @@ exports.getLogin = (req, res, next) => {
     // .trim()
     // .split('=')[1] === 'true';
 
-    console.log(req.session.isLoggedIn);
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    }
+    else {
+        message = null;
+    }
 
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
+        pageTitle: 'Login',
+        errorMessage: message
     });
 };
 
 
 exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    }
+    else {
+        message = null;
+    }
+
     res.render('auth/signup', {
         path: '/signup',
-        pageTitle: 'Signup'
+        pageTitle: 'Signup',
+        errorMessage: message
     })
 }
 
@@ -34,10 +50,10 @@ exports.postLogin = (req, res, next) => {
     // User.findById('637b8ce105699be0c5cf7817')
     User.findOne({ email: email })
         .then(user => {
-            //Basic route protection commented now because middleware path implemented
-            // if (!user) {
-            //     return res.redirect('/login');
-            // }
+            if (!user) {
+                req.flash('error', 'Invalid email or password.')
+                return res.redirect('/login');
+            }
             bcrypt.compare(password, user.password).then(
                 doMatch => {
                     console.log(doMatch)
@@ -45,12 +61,13 @@ exports.postLogin = (req, res, next) => {
                         // if doMatch is true, passwords are equal
                         req.session.user = user;
                         req.session.isLoggedIn = true;
-                        
+
                         return req.session.save((err) => {
                             console.log(err);
                             res.redirect('/');
                         })
                     }
+                    req.flash('error', 'Invalid email or password.')
                     res.redirect('/login');
                 }
             ).catch(err => {
@@ -68,6 +85,7 @@ exports.postSignup = (req, res, next) => {
         .then(
             userDoc => {
                 if (userDoc) {
+                    req.flash('error', 'E-mail already exists, please pick a different one')
                     return res.redirect('/signup')
                 }
                 return bcrypt.hash(password, 12)
