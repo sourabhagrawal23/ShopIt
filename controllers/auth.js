dfcb9cbb9cb0093a946d55452684e300958ecd5c
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-07 17:40:07 +0530

    Email validations added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer')
 const sendgridTransport = require('nodemailer-sendgrid-transport')
+const { validationResult } = require('express-validator/check');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
@@ -90,6 +91,16 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array()[0].msg
+        })
+    }
+
     User.findOne({ email: email })
         .then(
             userDoc => {
