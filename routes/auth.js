8d8280d8a3e13e4dbd9ac321265527776a1715d1
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-07 17:48:33 +0530

    Password validations added

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,43 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
-const authController = require('../controllers/auth')
+const authController = require('../controllers/auth');
 
-//This route is like a mini Express app which can be tied to the other Express apps
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin)
+router.post('/login', authController.postLogin);
 
 router.post(
-    '/signup', 
+  '/signup',
+  [
     check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email')
-    .custom((value, {req}) => {
-        if(value === 'test@test.com')
-        {
-            throw new Error('This email addess is forbidden')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        if (value === 'test@test.com') {
+          throw new Error('This email address if forbidden.');
         }
-        return true
-    }), 
-    authController.postSignup);
+        return true;
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters.'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords have to match!');
+      }
+      return true;
+    })
+  ],
+  authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
