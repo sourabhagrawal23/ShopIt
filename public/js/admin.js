8a436c34e51a15af59aec1998255c6936405632c
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-13 01:09:40 +0530

    bug fix

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,23 +1,24 @@
-const deleteProduct = (btn) => {
+const deleteProduct = btn => {
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
-
+  
     const productElement = btn.closest('article');
-
+  
     fetch('/admin/product/' + prodId, {
-        method: 'DELETE',
-        headers: {
-            'csrf-token': csrf
-        }
+      method: 'DELETE',
+      headers: {
+        'csrf-token': csrf
+      }
     })
-    .then(result => {
+      .then(result => {
         return result.json();
-    })
-    .then(data => {
+      })
+      .then(data => {
         console.log(data);
-        productELement.parentNode.removeChild(productElement);
-    })
-    .catch(err => {
-        console.log(data);
-    })
-};
+        productElement.parentNode.removeChild(productElement);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  
--- util/file.js
+++ util/file.js
@@ -8,5 +8,4 @@ const deleteFile = (filePath) => {
     })
 }
 
-
 exports.deleteFile = deleteFile;
