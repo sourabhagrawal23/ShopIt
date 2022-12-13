9fd90ea37bb720a8fb04f90332d69e7050e107c2
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-13 14:55:42 +0530

    Stripe payments implementation

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,8 +4,11 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 const product = require('../models/product');
 const fs = require('fs');
-const path = require('path')
-const PDFDocument = require('pdfkit')
+const path = require('path');
+const PDFDocument = require('pdfkit');
+const { request } = require('http');
+//Add secret key from stripe here
+const stripe = require('stripe')('');
 
 const ITEMS_PER_PAGE = 2;
 
@@ -345,6 +348,38 @@ exports.postOrder = (req, res, next) => {
 };
 
 
+exports.getCheckoutSuccess = (req, res, next) => {
+    req.user
+        .populate('cart.items.productId')
+        //   .execPopulate()
+        .then(user => {
+            console.log("Sourabh");
+
+            const products = user.cart.items.map(i => {
+                return { quantity: i.quantity, product: { ...i.productId._doc } };
+            });
+            const order = new Order({
+                user: {
+                    email: req.user.email,
+                    userId: req.user
+                },
+                products: products
+            });
+            return order.save();
+        })
+        .then(result => {
+            return req.user.clearCart();
+        })
+        .then(() => {
+            res.redirect('/orders');
+        })
+        .catch(err => {
+            const error = new Error(err)
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
 //sequelize code
 // exports.getOrders = (req, res, next) => {
 //     req.user
@@ -377,10 +412,49 @@ exports.getOrders = (req, res, next) => {
 
 
 exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout', {
-        path: '/checkout',
-        pageTitle: 'Checkout'
-    })
+    let products;
+    let total = 0;
+
+    req.user
+        .populate("cart.items.productId")
+        // .execPopulate()
+        // .getCart()
+        .then(user => {
+            products = user.cart.items;
+            // console.log(product);
+            let total = 0;
+            products.forEach(p => {
+                total += p.quantity * p.productId.price;
+            })
+            return stripe.checkout.sessions.create({
+                payment_method_type: ['card'],
+                line_items: products.map(p => {
+                    return {
+                        name: p.productId.title,
+                        description: p.productId.description,
+                        amount: p.productId.price * 100,
+                        currency: 'usd',
+                        quantity: p.quantity
+                    };
+                }),
+                success_url: request.protocol + '://' +req.get('host') + '/checkout/success',
+                cancel_url: request.protocol + '://' +req.get('host') + '/checkout/cancel'
+            });
+        })
+        .then(session => {
+            res.render('shop/checkout', {
+                path: '/checkout',
+                pageTitle: 'Checkout',
+                products: products,
+                totalSum: total,
+                sessionId: session.id
+            });
+        })
+        .catch(err => {
+            const error = new Error(err)
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 }
 
 exports.getInvoice = (req, res, next) => {
